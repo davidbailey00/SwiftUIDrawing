a3fc2b75c0d89a90845199e18671d9290fec2af3
David Bailey <davidbailey00@outlook.com>
2021-06-10 17:23:25 +0100

    Create spiral pattern with adjustable offset + width

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,52 +7,61 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
+struct Flower: Shape {
+    var petalOffset: Double = -20
+    var petalWidth: Double = 100
+
     func path(in rect: CGRect) -> Path {
         var path = Path()
 
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        for number in stride(
+            from: CGFloat.zero,
+            to: CGFloat.pi * 2,
+            by: CGFloat.pi / 8
+        ) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+
+            let position = rotation.concatenating(
+                CGAffineTransform(
+                    translationX: rect.width / 2,
+                    y: rect.height / 2
+                )
+            )
+
+            let originalPetal = Path(
+                ellipseIn: CGRect(
+                    x: CGFloat(petalOffset),
+                    y: 0,
+                    width: CGFloat(petalWidth),
+                    height: rect.width / 2
+                )
+            )
+
+            let rotatedPetal = originalPetal.applying(position)
+            path.addPath(rotatedPetal)
+        }
 
         return path
-    }
-}
-
-struct Arc: InsettableShape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
-    var insetAmount: CGFloat = 0
-
-    func path(in rect: CGRect) -> Path {
-        let adjustment = Angle.degrees(90)
-
-        var path = Path()
-        path.addArc(
-            center: CGPoint(x: rect.midX, y: rect.midY),
-            radius: rect.width / 2 - insetAmount,
-            startAngle: startAngle - adjustment,
-            endAngle: endAngle - adjustment,
-            clockwise: !clockwise
-        )
-
-        return path
-    }
-
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
     }
 }
 
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+
     var body: some View {
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(Color.blue, lineWidth: 40)
-            .padding()
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(Color.red, style: FillStyle(eoFill: true))
+
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding(.bottom)
+
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+        }
+        .padding()
     }
 }
 
